4cefe3f219c46fee311e2fff236afeb0b2f20647
Simon Liotier <simon.liotier@iadvize.com>
2020-09-29 16:07:10 +0200

    Add comments

--- XCUITests_UIImagePickerController_BitriseUITests/XCUITests_UIImagePickerController_BitriseUITests.swift
+++ XCUITests_UIImagePickerController_BitriseUITests/XCUITests_UIImagePickerController_BitriseUITests.swift
@@ -11,12 +11,15 @@ class XCUITests_UIImagePickerController_BitriseUITests: XCTestCase {
         let app = XCUIApplication()
         app.launch()
 
+        // Tap the "Pick image" button that will trigger the image picker.
         let pickImageButton = app.buttons["Pick image"]
         pickImageButton.tap()
 
+        // The image picker displays a collection of images whose names look like: "Photo, March 30, 2018, 9:14 PM".
+        // We pick the first photo.
         let predicate = NSPredicate(format: "label CONTAINS %@", "Photo")
         let photo = app.images.containing(predicate).firstMatch
-        XCTAssert(photo.waitForExistence(timeout: 10))
+        XCTAssert(photo.waitForExistence(timeout: 10)) // Generous timeout in case the image picker takes time to open.
         photo.tap()
     }
 }
